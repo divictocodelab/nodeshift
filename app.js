871027a81de4ae0659bc1208edfacc6cf8716fbd
Divya <divya.mahajan@monstar-lab.com.sg>
2019-11-14 15:27:12 +0530

    add openshift template

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var Server = require('net').Server;
+var os = require("os");
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,18 +20,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+var hostname = os.hostname();
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+var listener = app.listen(8882, function () {
+  console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.use(function (req, res, next) {
+  var host = req.get('host');
+  console.log(app.get('url'));
+});
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
